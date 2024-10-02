5c17b59987f22c3ea663860dc66146edba03a46d
Josh Urdnb <joshua.urdanabia@gmail.com>
2024-10-02 19:22:20 +0200

    "Added tests for modal opening and mocked the store"

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -7,8 +7,13 @@ import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store"
 
 import router from "../app/Router.js";
+import Bills from "../containers/Bills.js";
+import userEvent from "@testing-library/user-event";
+
+jest.mock("../app/store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -34,6 +39,35 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+  })
+
+  describe('Given I am connected as an employee and I am on Bills page and I clicked on the icon', () => {
+    describe('When I click on the icon eye', () => {
+      test('A modal should open', () => {
+        Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+        window.localStorage.setItem('user', JSON.stringify({
+          type: 'Employee'
+        }))
+        document.body.innerHTML = BillsUI({data:bills})
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        const store = null
+        const bill = new Bills({
+          document, onNavigate, store, localStorage: window.localStorage
+        })
+  
+        $.fn.modal = jest.fn()
+        const eye = screen.getAllByTestId('icon-eye')[0]
+        const handleClickIconEye = jest.fn(bill.handleClickIconEye(eye))
+        eye.addEventListener('click', handleClickIconEye)
+        userEvent.click(eye)
+        expect(handleClickIconEye).toHaveBeenCalled()
+  
+        const modale = document.getElementById('modaleFile')
+        expect(modale).toBeTruthy()
+      })
     })
   })
 })
