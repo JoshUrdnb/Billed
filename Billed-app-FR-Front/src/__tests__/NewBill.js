9ef884619b39a900dc373af4a97ee18b36d248fe
Josh Urdnb <joshua.urdanabia@gmail.com>
2024-10-10 15:47:43 +0200

    Add test to verify bill icon is highlighted on NewBill page for employee user.

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -1,18 +1,35 @@
 /**
  * @jest-environment jsdom
  */
-
-import { screen } from "@testing-library/dom"
+import {screen, waitFor} from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import mockStore from "../__mocks__/store"
+import router from "../app/Router.js";
+import userEvent from "@testing-library/user-event";
+import { ROUTES_PATH } from "../constants/routes.js";
 
+
+jest.mock("../app/store", () => mockStore)
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
+  describe("When I am on Bills Page", () => {
+    test("Then bill icon in vertical layout should be highlighted", async () => {
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.NewBill)
+      await waitFor(() => screen.getByTestId('icon-mail'))
+      const windowIcon = screen.getByTestId('icon-mail')
+      // Test que l'icône des factures est bien surlignée
+      expect(windowIcon.classList.contains('active-icon')).toBe(true)
     })
   })
 })
