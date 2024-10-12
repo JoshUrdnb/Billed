97027a8e3c9bd58ed56211f3d3b1b9f0a4af15fe
Josh Urdnb <joshua.urdanabia@gmail.com>
2024-10-12 14:07:07 +0200

    "Verify all form inputs are present on NewBill page."

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-import {screen, waitFor} from "@testing-library/dom"
+import {screen, waitFor, fireEvent} from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import { localStorageMock } from "../__mocks__/localStorage.js";
@@ -32,4 +32,32 @@ describe("Given I am connected as an employee", () => {
       expect(windowIcon.classList.contains('active-icon')).toBe(true)
     })
   })
+
+  describe("When I am on NewBill Page", () => {
+    test("Then all form inputs should be present", async () => {
+      // Setup: Render the NewBill page UI
+      document.body.innerHTML = NewBillUI()
+
+      // Vérifier que tous les inputs nécessaires sont présents
+      const expenseType = screen.getByTestId("expense-type")
+      const expenseName = screen.getByTestId("expense-name")
+      const datepicker = screen.getByTestId("datepicker")
+      const amount = screen.getByTestId("amount")
+      const vat = screen.getByTestId("vat")
+      const pct = screen.getByTestId("pct")
+      const commentary = screen.getByTestId("commentary")
+      const file = screen.getByTestId("file")
+
+      // Tests pour s'assurer que chaque élément est présent dans le DOM
+      expect(expenseType).toBeTruthy()
+      expect(expenseName).toBeTruthy()
+      expect(datepicker).toBeTruthy()
+      expect(amount).toBeTruthy()
+      expect(vat).toBeTruthy()
+      expect(pct).toBeTruthy()
+      expect(commentary).toBeTruthy()
+      expect(file).toBeTruthy()
+    })
+  })
+
 })
